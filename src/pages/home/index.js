3d0b532834d3fdc15da9bbeb1257623a3713fa05
Wadhera, Siddhant (US - Bengaluru) <swadhera@deloitte.com>
2021-01-14 13:42:50 +0530

    base set Up with linting , prettier and network layer

---
+++ src/pages/home/index.js
@@ -0,0 +1,11 @@
+// Network
+//  import NetworkUtils from '../../network';
+
+// Styles
+import './styles.scss';
+
+const Home = () => {
+	return <div className="bokl-homepage d-flex">Landing Page of Books List</div>;
+};
+
+export default Home;
